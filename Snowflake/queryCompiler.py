52617f951472ac2d28ae19c5ae48722dbf6c44bf
cyndizzz <czhong@videoamp.com>
2022-05-06 19:58:06 -0400

    python module to execute .sql file

---
+++ Snowflake/queryCompiler.py
@@ -0,0 +1,20 @@
+from snowflake.connector import ProgrammingError
+
+def sql_execute(filename,cur):
+    fd=open(filename, 'r')
+    sqlFile=fd.read()
+    fd.close()
+    # print(sqlFile)
+
+    sqlCommands=sqlFile.split(';')
+    # display(sqlCommands)
+
+    for command in sqlCommands:
+        print(command)
+        try:
+            cur.execute(command)
+            rows = cur.fetchall()
+            print(rows)
+        except ProgrammingError as e:
+            print("Error: ", e)
+            print("Command skipped: ")
